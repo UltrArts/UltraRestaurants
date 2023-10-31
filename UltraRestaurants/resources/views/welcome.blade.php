840979c94696da105bdec2893ceba98dc1dcb62c
UltrArts <edsonleandrodacruz@gmail.com>
2023-10-31 16:56:37 +0200

    Setting Up database

---
+++ UltraRestaurants/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="{{ asset('build/assets/bootstrap/bootstrap.css') }}" rel="stylesheet">
+        <link href="{{ asset('build/assets/fontawesome/all.css') }}" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+    </body>
+    <script src="{{ asset('build/assets/bootstrap/bootstrap.js') }}"></script>
+        <script src="{{ asset('build/assets/fontawesome/all.js') }}"></script>
+
+</html>
