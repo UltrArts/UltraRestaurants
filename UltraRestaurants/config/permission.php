0d59aad7670114bf28c7ae5b90427a24b8c10697
UltrArts <edsonleandrodacruz@gmail.com>
2023-11-02 16:59:29 +0200

    Some changes

--- UltraRestaurants/config/permission.php
+++ UltraRestaurants/config/permission.php
@@ -121,7 +121,7 @@ return [
      * (view the latest version of this package's migration file)
      */
 
-    'teams' => false,
+    'teams' => true,
 
     /*
      * Passport Client Credentials Grant
